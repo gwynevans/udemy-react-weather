e41c54d869eab9cf4520c909c8d8ebd025ff7bc2
gwyn <gwyn.evans@gmail.com>
2016-05-02 18:06:39 +0100

    server tweaks

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ var express = require('express');
 
 // Create our app
 var app = express();
-const PORT = process.env.PORT || 3000;
 
-// app.use(function (req, res, next) {
-//   if (req.protocol === 'http') {
-//     next();
-//   } else {
-//     res.redirect('http://' + req.hostname + req.url);
-//   }
-// });
+var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+app.use(function (req, res, next) {
+  if (req.protocol === 'http') {
+    next()
+  } else {
+    res.redirect('http://' + req.hostname + req.url)
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(PORT, function() {
-  console.log('Express server is up on port ' + PORT);
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
 });
