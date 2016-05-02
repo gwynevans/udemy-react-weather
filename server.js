a26066940956a63545032da9f2bcee36c1a8f4fc
gwyn <gwyn.evans@gmail.com>
2016-05-02 19:14:08 +0100

    Try alternative server.js

--- server.js
+++ server.js
@@ -3,19 +3,18 @@ var express = require('express');
 // Create our app
 var app = express();
 
-var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
+const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
-  if (req.protocol === 'http') {
-    next()
+  if (req.headers['x-forwarded-proto'] == 'http') {
+    next();
   } else {
-    res.redirect('http://' + req.hostname + req.url)
+    res.redirect('http://' + req.hostname + req.url);
   }
 });
 
 app.use(express.static('public'));
 
-app.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+app.listen(PORT, function() {
+    console.log('Listening on port %d', PORT);
 });
